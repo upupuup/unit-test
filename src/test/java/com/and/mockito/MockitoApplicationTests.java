50948b9710c93d2f0ffb2ab41fc2a8aa464ef030
jiangzhihong <woyaojiayou_a@163.com>
2021-06-20 13:09:59 +0800

    提交单元测试的代码

---
+++ src/test/java/com/and/mockito/MockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.and.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
